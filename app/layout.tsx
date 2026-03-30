59a6c3d1019cf752f546bcae95817a816acd081b
Krish Deshmukh <deshmukh.102@osu.edu>
2026-03-29 23:38:14 -0400

    Install Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,8 @@ export const metadata: Metadata = {
   },
 };
 
+import { Analytics } from "@vercel/analytics/next"
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -16,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-[100dvh] antialiased">
-      <body className="h-full flex flex-col overflow-hidden select-none">{children}</body>
+      <body className="h-full flex flex-col overflow-hidden select-none">
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
